bccbbc6df56627093cfdf0554682c4be4ffe65e8
Paul Pucciarelli <paul@ticketevolution.com>
2015-06-14 22:01:00 -0400

    Work on login.

--- README.md
+++ README.md
@@ -5,4 +5,19 @@ A fullstack app skeleton for web &amp; mobile apps. PostgreSQL, REST, React, Rea
 https://github.com/sails101/using-passport
 http://miscposts1.blogspot.com/2014/09/sailsjs-user-authentication.html
 http://iliketomatoes.com/implement-passport-js-authentication-with-sails-js-0-10-2/
+Running with forever:
+https://github.com/balderdashy/sails/issues/1334
 
+# App Server
+// start app server
+forever start --watch app.js
+
+# Database
+```sql
+
+// start db
+pg_ctl -D /usr/local/var/postgres/data -l logfile start
+
+// stop db
+pg_ctl -D /usr/local/var/postgres/data -l logfile stop
+```
--- backend/api/controllers/AuthController.js
+++ backend/api/controllers/AuthController.js
@@ -9,15 +9,27 @@ module.exports = {
   },
 
   login: function(req, res) {
+    // http://passportjs.org/docs/authenticate
     passport.authenticate('local', function(err, user, info) {
-      if ((err) || (!user)) {
-        return res.send({
-          message: info.message,
+      if (err) {
+        return res.serverError({
+          error: err,
+          user: user
+        });
+      }
+      if (!user) {
+        return res.serverError({
+          error: info.message,
           user: user
         });
       }
       req.logIn(user, function(err) {
-        if (err) res.send(err);
+        if (err) {
+          return res.serverError({
+            error: err,
+            user: user
+          });
+        }
         return res.send({
           message: info.message,
           user: user
